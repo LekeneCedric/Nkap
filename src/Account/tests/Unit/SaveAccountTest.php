3ec1934f039cdc732c1154feec311826026987af
HackerMan237 <93648451+LekeneCedric@users.noreply.github.com>
2023-10-24 08:16:10 +0100

    start test_can_save_account

---
+++ src/Account/tests/Unit/SaveAccountTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Code237\Nkap\Account\tests\Unit;
+
+class SaveAccountTest
+{
+    public function test_can_save_account()
+    {
+        $account = AccountSUT::asSUT();
+    }
+}
